9dd7008aec28673035213d0023565a58bf13ae99
James Monger <jameskmonger@hotmail.co.uk>
2016-10-30 11:45:04 +0000

    Pass SingleDigitTranslator tests

--- src/main/java/translators/SingleDigitTranslator.java
+++ src/main/java/translators/SingleDigitTranslator.java
@@ -2,9 +2,14 @@ package main.java.translators;
 
 public class SingleDigitTranslator implements NumberTranslator {
 
+	static final String[] DIGITS = {
+		"one", "two", "three", "four", "five",
+		"six", "seven", "eight", "nine"
+	};
+	
 	@Override
 	public String translateNumber(int number) {
-		return null;
+		return DIGITS[number - 1];
 	}
 
 }
